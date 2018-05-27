bd751bcc28d15510bd138007a051e51c61d91770
Loicbidaux <34678703+Loicbidaux@users.noreply.github.com>
2018-05-28 01:14:04 +0200

    Mise en place de l'interface graphique
    Ajout de l'écran d'accueil et de la carte.
    Mise en place de la fonction de plein écran.
    Prochaine étape : ajout des unité sur la carte.

--- Risk/src/Interface.java
+++ Risk/src/Interface.java
@@ -1,11 +1,17 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,9 +19,11 @@ import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
 public class Interface extends JFrame {
-
+	
+	
 	private JPanel contentPane;
-
+	private boolean FSflag = false;
+    private int PrevX, PrevY, PrevWidth, PrevHeight;
 	/**
 	 * Launch the application.
 	 */
@@ -25,6 +33,7 @@ public class Interface extends JFrame {
 				try {
 					Interface frame = new Interface();
 					frame.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,62 +44,126 @@ public class Interface extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
 	public Interface() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0, 0, 655, 518);
+		setBounds(0, 0, 1920, 1080);
 		
-		contentPane = new JPanel();
+		
+		
+		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		
 		JLabel lblTest = new JLabel("test");
-		lblTest.setBounds(0, 0, 640, 480);
-		lblTest.setIcon(new ImageIcon("src/Images/Menu/menu1.png"));
+		lblTest.setBounds(0, 0, 1920, 1080);
+		lblTest.setIcon(new ImageIcon("src/Images/Menu/fond.png"));
 		contentPane.add(lblTest);
 		
 		JLabel lblTest2 = new JLabel("test2");
-		lblTest2.setBounds(0, 0, 640, 480);
-		lblTest2.setIcon(new ImageIcon("src/Images/Menu/menu1.png"));
-		
+		lblTest2.setBounds(0, 0, 1920, 1080);
+		lblTest2.setIcon(new ImageIcon("src/Images/carte/BASE-RISK-STARWARS.png"));
+		contentPane.add(lblTest2);
 		
 		
 		Border border = BorderFactory.createLineBorder(Color.red, 5);
-
-		JLabel btnPlay = new JLabel("play");
-		btnPlay.setBounds(196, 95, 256, 26);
-		btnPlay.setBorder(border);
-		lblTest.add(btnPlay);
 		
-		btnPlay.addMouseListener(new MouseListener() {
-			
+		JLabel btnPlay = new JLabel("play");
+		lblTest.add(btnPlay);
+		btnPlay.setBounds(707, 470, 505, 145);
+		btnPlay.setIcon(new ImageIcon("src/Images/Test/btnJouer.png"));
+		
+		JButton btnFullscreen = new JButton("FullScreen");
+		lblTest.add(btnFullscreen);
+		btnFullscreen.setOpaque(false);
+		btnFullscreen.setContentAreaFilled(false); // On met à false pour empêcher le composant de peindre l'intérieur du JButton.
+		btnFullscreen.setBorderPainted(false); // De même, on ne veut pas afficher les bordures.
+		btnFullscreen.setFocusPainted(false); // On n'affiche pas l'effet de focus.
+		btnFullscreen.setBounds(919, 650, 83, 81);
+		btnFullscreen.setIcon(new ImageIcon("src/Images/icone/fullscreen.png"));
+		btnFullscreen.setVisible(true);
+
+		JButton btnFullscreenCarte = new JButton("FullScreenCarte");
+		lblTest2.add(btnFullscreenCarte);
+		btnFullscreenCarte.setOpaque(false);
+		btnFullscreenCarte.setContentAreaFilled(false); // On met à false pour empêcher le composant de peindre l'intérieur du JButton.
+		btnFullscreenCarte.setBorderPainted(false); // De même, on ne veut pas afficher les bordures.
+		btnFullscreenCarte.setFocusPainted(false); // On n'affiche pas l'effet de focus.
+		btnFullscreenCarte.setBounds(10, 990, 83, 81);
+		btnFullscreenCarte.setIcon(new ImageIcon("src/Images/icone/fullscreen.png"));
+		btnFullscreenCarte.setVisible(true);
+		
+		
+		
+		
+		MouseListener fullscreen = new MouseListener() {
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+				if (FSflag == false) {
+	                PrevX = getX();
+	                PrevY = getY();
+	                PrevWidth = getWidth();
+	                PrevHeight = getHeight();
+
+	                // Destroys the whole JFrame but keeps organized every Component
+	                // Needed if you want to use Undecorated JFrame dispose() is the
+	                // reason that this trick doesn't work with videos.
+	                dispose();
+	                setUndecorated(true);
+
+	                setBounds(0, 0, getToolkit().getScreenSize().width,
+	                        getToolkit().getScreenSize().height);
+	                setVisible(true);
+	                btnFullscreen.setIcon(new ImageIcon("src/Images/icone/unfullscreen.png"));
+	                btnFullscreenCarte.setIcon(new ImageIcon("src/Images/icone/unfullscreen.png"));
+	                FSflag = true;
+	            } else {
+	                setVisible(true);
+	                setBounds(PrevX, PrevY, PrevWidth, PrevHeight);
+	                dispose();
+	                setUndecorated(false);
+	                setVisible(true);
+	                btnFullscreen.setIcon(new ImageIcon("src/Images/icone/fullscreen.png"));
+	                btnFullscreenCarte.setIcon(new ImageIcon("src/Images/icone/fullscreen.png"));
+	                FSflag = false;
+	            }
 			}
-			
+
 			@Override
-			public void mousePressed(MouseEvent e) {
+			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
 				
 			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				// TODO Auto-generated method stub
 				
 			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			};
+			
+			
+		btnPlay.addMouseListener(new MouseListener() {
 			
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
 				// TODO Auto-generated method stub
 				lblTest.setVisible(false);
 				contentPane.remove(lblTest);
@@ -98,8 +171,39 @@ public class Interface extends JFrame {
 				contentPane.validate();
 				System.out.println("ok");
 			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
 		});
 		
-		
+		btnFullscreen.addMouseListener(fullscreen);
+		btnFullscreenCarte.addMouseListener(fullscreen);
+
+	
+	
+	}	
 	}
-	}
+	
+
+
